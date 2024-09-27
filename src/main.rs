530060ea5e0f92455289a0d9ee930b5c906e6062
emotionalzz <112372170+emotionalzz@users.noreply.github.com>
2024-09-26 20:24:21 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,9 @@ fn main() {
     const N_STEP: usize = 1000;
     const N_DIM: usize = 2;
     // Fill this
+    let mut state = State::<N_DIM>::new(0xdeadbeef);
+    for _ in 0..N_STEP {
+        state.take_step();
+        println!("{:?}, {:?}", state.arr[0], state.arr[1]);
+    }
 }
